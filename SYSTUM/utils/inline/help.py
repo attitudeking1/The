28f314c2692033f70498177a32d43656ccb82bdc
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-02-18 14:18:52 +0530

    Update help.py

--- SYSTUM/utils/inline/help.py
+++ SYSTUM/utils/inline/help.py
@@ -25,7 +25,6 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
-                                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb6",
