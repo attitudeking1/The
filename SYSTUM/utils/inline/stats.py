597ca4d8a104b54352ffc2f53170a958ad915d1c
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-02-18 14:44:33 +0530

    Update stats.py

--- SYSTUM/utils/inline/stats.py
+++ SYSTUM/utils/inline/stats.py
@@ -2,7 +2,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def stats_buttons(_, status):
-    not_sudo = "restricted cmd"
     sudo = [
         InlineKeyboardButton(
             text=_["SA_B_2"],
@@ -15,7 +14,7 @@ def stats_buttons(_, status):
     ]
     upl = InlineKeyboardMarkup(
         [
-            sudo if status else not_sudo,
+            sudo if status else,
             [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
